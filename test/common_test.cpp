a31e2fd7bb2158f5b8f00b9693d7a0040eeaac8f
Lukas Heyn <lukas.heyn@gmail.com>
2024-03-10 14:25:43 +0100

    first things + tests

--- test/common_test.cpp
+++ test/common_test.cpp
@@ -1,5 +1,17 @@
 #include <common.h>
 
+#include <unordered_map>
+
 auto main() -> int {
 	if (auto arr = common::get_array_from_tuple(std::make_tuple(1, 2, 3)); arr != std::array{1, 2, 3}) return 1;
+
+	{
+		std::unordered_map<std::array<int, 2>, int, common::ArrayHasher> m;
+		if (m[{1, 2}] = 3; m[{1, 2}] != 3) return 1;
+	}
+
+	{
+		common::pair_priority_queue<int, std::array<int, 2>> pq;
+		if (pq.push(std::make_pair(1, std::array{2, 3})); pq.top().first != 1 || pq.top().second != std::array{2, 3}) return 1;
+	}
 }
