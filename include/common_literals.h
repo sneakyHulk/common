c54d884ffef2e45e77a8848df7fd89698690b582
Lukas <sneakyHulk@users.noreply.github.com>
2024-05-28 17:15:53 +0200

    Update common_literals.h

--- include/common_literals.h
+++ include/common_literals.h
@@ -1,3 +1,5 @@
 #pragma once
+#include <cstdint>
 
-[[maybe_unused]] constexpr char operator""_ch(unsigned long long int num) { return num; }
+[[maybe_unused]] inline constexpr char operator""_ch(unsigned long long int num) { return num; }
+[[maybe_unused]] inline constexpr std::uint8_t operator""_u8(unsigned long long int num) { return  static_cast<std::uint8_t>(num); }
