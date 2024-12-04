d559ac2f223e4403d6931bcb4597c1c0d32c4cc6
Lukas Heyn <lukas.heyn@gmail.com>
2024-12-04 13:41:46 +0100

    common update

--- include/common_output.h
+++ include/common_output.h
@@ -1,18 +1,16 @@
 #pragma once
 
 #include <common.h>
+#include <common_exception.h>
 #include <common_ostream.h>
 
+#include <filesystem>
 #include <iostream>
+#include <source_location>
 #include <sstream>
 
 namespace common {
-#ifdef __cpp_concepts
-	template <typename T>
-	concept printable = requires(std::ostream& os, const T& msg) {
-		{ os << msg };
-	};
-#endif
+
 
 #ifdef __cpp_concepts
 	[[maybe_unused]] std::string stringprint(printable auto&&... args) {
@@ -76,12 +74,19 @@ namespace common {
 #endif
 
 #ifdef __cpp_concepts
+	template <printable... Args>
+	struct print_loc {
+		explicit print_loc(Args&&... args, std::source_location const location = std::source_location::current()) { ((std::cout << '[' << std::filesystem::path(location.file_name()).stem().string() << "]: ") << ... << args) << std::flush; }
+	};
+
+	template <printable... Args>
+	print_loc(Args&&... args) -> print_loc<Args...>;
+
 	[[maybe_unused]] void print(printable auto&&... args) {
 #else
 	template <typename... T>
 	[[maybe_unused]] void print(T&&... args) {
 #endif
-		std::ostringstream ss;
 		(std::cout << ... << args) << std::flush;
 	}
 
@@ -128,14 +133,80 @@ namespace common {
 		std::cout << e;
 	}
 #endif
+	enum class LOGLEVEL {
+		DEBUG,
+		INFO,
+		WARNING,
+		ERROR,
+		CRITICAL,
+	};
+
+#define BLK "\033[0;30m"
+#define RED "\033[0;31m"
+#define GRN "\033e[0;32m"
+#define YEL "\033[0;33m"
+#define BLU "\033[0;34m"
+#define MAG "\033[0;35m"
+#define CYN "\033[0;36m"
+#define WHT "\033[0;37m"
+#define RESET "\033[0m"
 
 #ifdef __cpp_concepts
+	template <printable... Args>
+	struct println_loc {
+		explicit println_loc(Args&&... args, std::source_location const location = std::source_location::current()) {
+			((std::cout << '[' << std::filesystem::path(location.file_name()).stem().string() << "]: ") << ... << args) << std::endl;
+		}
+	};
+
+	template <printable... Args>
+	println_loc(Args&&... args) -> println_loc<Args...>;
+
+	template <printable... Args>
+	struct println_warn_loc {
+		explicit println_warn_loc(Args&&... args, std::source_location const location = std::source_location::current()) {
+			((std::cout << YEL << '[' << std::filesystem::path(location.file_name()).stem().string() << "]: ") << ... << args) << RESET << std::endl;
+		}
+	};
+
+	template <printable... Args>
+	println_warn_loc(Args&&... args) -> println_warn_loc<Args...>;
+
+	void println_warn(printable auto&&... args) { ((std::cout << YEL) << ... << args) << RESET << std::endl; }
+
+	template <printable... Args>
+	struct println_error_loc {
+		explicit println_error_loc(Args&&... args, std::source_location const location = std::source_location::current()) {
+			((std::cout << RED << '[' << std::filesystem::path(location.file_name()).stem().string() << "]: ") << ... << args) << RESET << std::endl;
+		}
+	};
+
+	template <printable... Args>
+	println_error_loc(Args&&... args) -> println_error_loc<Args...>;
+
+	void println_error(printable auto&&... args) { ((std::cout << RED) << ... << args) << RESET << std::endl; }
+
+	template <printable... Args>
+	struct println_critical_loc {
+		explicit println_critical_loc(Args&&... args, std::source_location const location = std::source_location::current()) {
+			((std::cout << RED << '[' << std::filesystem::path(location.file_name()).stem().string() << "]: ") << ... << args) << RESET << std::endl;
+			throw Exception(std::forward<decltype(args)>(args)...);
+		}
+	};
+
+	template <printable... Args>
+	println_critical_loc(Args&&... args) -> println_critical_loc<Args...>;
+
+	void println_critical(printable auto&&... args) {
+		((std::cout << RED) << ... << args) << RESET << std::endl;
+		throw Exception(std::forward<decltype(args)>(args)...);
+	}
+
 	[[maybe_unused]] void println(printable auto&&... args) {
 #else
 	template <typename... T>
 	[[maybe_unused]] void println(T&&... args) {
 #endif
-		std::ostringstream ss;
 		(std::cout << ... << args) << std::endl;
 	}
 
