0264ce8595d12e6021e25c01be578721d9190e69
Lukas Heyn <lukas.heyn@gmail.com>
2026-03-16 10:14:03 +0100

    linspace update

--- include/common_linspace.h
+++ include/common_linspace.h
@@ -1,16 +1,32 @@
 #pragma once
 #include <array>
 #include <cstdint>
+#include <span>
 
 #if defined(__cpp_lib_ranges_enumerate)
 #include <ranges>
 #endif
 
 namespace common {
-	template <std::size_t N>
-	std::array<double, N> linspace(double from, double to) {
-		std::array<double, N> result;
+	template <std::floating_point T, std::size_t N>
+	std::array<T, N> linspace(T from, T to) {
+		std::array<T, N> result;
 
+#if defined(__cpp_lib_ranges_enumerate)
+		for (T const step = (to - from) / (N - 1); auto const& [i, e] : std::ranges::views::enumerate(result)) {
+#else
+		auto i = -1;
+		for (T const step = (to - from) / (N - 1); auto& e : result) {
+			++i;
+#endif
+			e = from + i * step;
+		}
+
+		return result;
+	}
+
+	template <std::floating_point T>
+	void linspace(T const from, T const to, std::size_t const N, std::span<T> result) {
 #if defined(__cpp_lib_ranges_enumerate)
 		for (double const step = (to - from) / (N - 1); auto const& [i, e] : std::ranges::views::enumerate(result)) {
 #else
@@ -20,19 +36,17 @@ namespace common {
 #endif
 			e = from + i * step;
 		}
-
-		return result;
 	}
 
-	template <std::size_t N>
-	std::array<double, N> linspace_exclusive(double from, double to) {
-		std::array<double, N> result;
+	template <std::floating_point T, std::size_t N>
+	std::array<T, N> linspace_exclusive(T from, T to) {
+		std::array<T, N> result;
 
 #if defined(__cpp_lib_ranges_enumerate)
-		for (double const step = (to - from) / N; auto const& [i, e] : std::ranges::views::enumerate(result)) {
+		for (T const step = (to - from) / N; auto const& [i, e] : std::ranges::views::enumerate(result)) {
 #else
 		auto i = -1;
-		for (double const step = (to - from) / N; auto& e : result) {
+		for (T const step = (to - from) / N; auto& e : result) {
 			++i;
 #endif
 			e = from + i * step;
--- test/test_common_linspace.cpp
+++ test/test_common_linspace.cpp
@@ -1,8 +1,14 @@
 #include <common_linspace.h>
 
+#include <vector>
+
 auto main() -> int {
-	if (auto const a = common::linspace<5>(0.0, 4.0); !(a[0] == 0.0 && a[1] == 1.0 && a[2] == 2.0 && a[3] == 3.0 && a[4] == 4.0)) return EXIT_FAILURE;
-	if (auto const a = common::linspace_exclusive<4>(0.0, 4.0); !(a[0] == 0.0 && a[1] == 1.0 && a[2] == 2.0 && a[3] == 3.0)) return EXIT_FAILURE;
+	if (auto const a = common::linspace<double, 5>(0.0, 4.0); !(a[0] == 0.0 && a[1] == 1.0 && a[2] == 2.0 && a[3] == 3.0 && a[4] == 4.0)) return EXIT_FAILURE;
+	if (auto const a = common::linspace_exclusive<double, 4>(0.0, 4.0); !(a[0] == 0.0 && a[1] == 1.0 && a[2] == 2.0 && a[3] == 3.0)) return EXIT_FAILURE;
+
+	std::vector<double> result(5);
+	common::linspace<double>(0.0, 4.0, 5, result);
+	if (auto const a = result; !(a[0] == 0.0 && a[1] == 1.0 && a[2] == 2.0 && a[3] == 3.0 && a[4] == 4.0)) return EXIT_FAILURE;
 
 	return EXIT_SUCCESS;
 }
